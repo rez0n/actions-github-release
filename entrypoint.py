0fa44cf6b130ade8b631a6d846fbca1e92fee112
Denis Verbin <r@lectra.me>
2020-11-04 20:15:49 +0200

    set outputs

--- entrypoint.py
+++ entrypoint.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from github import Github
 import os
 wanted_release = os.environ['wanted_release_type']
@@ -10,9 +12,9 @@ releases = repo.get_releases()
 for release in releases:
     if wanted_release == 'stable':
         if release.prerelease == 0 and release.draft == 0:
-            print(release.tag_name)
+            print('::set-output name=release::{}'.format(release.tag_name))
             break
     elif wanted_release == 'prerelease' or wanted_release == 'latest':
         if release.prerelease == 1:
-            print(release.tag_name)
+            print('::set-output name=release::{}'.format(release.tag_name))
             break
